47b37bba1d6ae7937c0523c917ebe3c1e0d2bd9c
pougem <pougem@pc-123-08.iut-valence.fr>
2012-10-23 14:44:36 +0200

    déplacement des classes inutilisées dans un autre répertoire

--- unused/BoiteAuxLettres.java
+++ unused/BoiteAuxLettres.java
@@ -1,9 +1,9 @@
-// FIXME trouver un nom plus évocateur pour le package (fr.iutvalence.java.projets.clientmail) ?
+// FIxME trouver un nom plus évocateur pour le package (fr.iutvalence.java.projets.clientmail) ?
 package fr.iutvalence.java.projet2.unused;
 
 import fr.iutvalence.java.projet2.Message;
 
-// FIXME corriger le commentaire (faire des phrases)
+// FIxME corriger le commentaire (faire des phrases)
 /** 
  * Cette classe contient les dossiers principaux du client mail : 
  * mails reçus, envoyés et supprimés (boite de réception, messages envoyés, corbeille) 
@@ -23,25 +23,25 @@ public class BoiteAuxLettres {
 	*/
 	private Message[] eltsEnvoyes;
 	
-	// FIXME il manque un attribut pour savoir combien il y a d'éléments dans le tableau
+	// FIxME il manque un attribut pour savoir combien il y a d'éléments dans le tableau
 	
 	/**
 	 * contient tous les mails supprimés par l'utilisateur
 	 */
 	private Message[] corbeille;
 
-	// FIXME il manque un attribut pour savoir combien il y a d'éléments dans le tableau
+	// FIxME il manque un attribut pour savoir combien il y a d'éléments dans le tableau
 	
-	// FIXME constructeurs, méthodes ?
+	// FIxME constructeurs, méthodes ?
 	
 	public BoiteAuxLettres()
 	{
-		// FIXME définir la capacité par défaut du tableau sous forme d'une constante
+		// FIxME définir la capacité par défaut du tableau sous forme d'une constante
 		this.eltsEnvoyes = new Message[100];
 		this.corbeille = new Message[100];
 		
-		// FIXME tous les attributs doivent être initialisés
+		// FIxME tous les attributs doivent être initialisés
 	}
 	
-	// FIXME méthodes ?
+	// FIxME méthodes ?
 }
--- unused/DossierAdresse.java
+++ unused/DossierAdresse.java
@@ -2,7 +2,7 @@ package fr.iutvalence.java.projet2.unused;
 
 
 
-// FIXME corriger le commentaire (écrire une phrase)
+// FIxME corriger le commentaire (écrire une phrase)
 /** 
  * Va contenir tous les dossiers relatifs à l'adresse mail DossierAdresse 
  * */
@@ -12,7 +12,7 @@ public class DossierAdresse
 	 * Définit le nom (donné par l'utilisateur lors de la création de la boite mail)
 	 * du dossier adresse mail
 	 */
-	// FIXME respecter les conventions d'écriture
+	// FIxME respecter les conventions d'écriture
 	private String nom_adresse_mail;
 	
 	/**
@@ -21,17 +21,17 @@ public class DossierAdresse
 	 */
 	private DossierMessages[] dossier;
 	
-	// FIXME il manque un attribut pour savoir combien il y a d'éléments dans le tableau
+	// FIxME il manque un attribut pour savoir combien il y a d'éléments dans le tableau
 	
 	public DossierAdresse(){
 		
-		// FIXME définir la capacité par défaut du tableau sous forme de constante
+		// FIxME définir la capacité par défaut du tableau sous forme de constante
 		this.dossier = new DossierMessages[20];
 		
-		// FIXME tous les attributs doivent être initialisés
+		// FIxME tous les attributs doivent être initialisés
 	
 	}
 	
-	// FIXME méthodes ?
+	// FIxME méthodes ?
 	
 }
